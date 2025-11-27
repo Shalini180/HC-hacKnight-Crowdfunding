02528c12672ad8100576dd862884287ffa46a902
unknown <shalini.a.annadurai@gmail.com>
2025-11-27 10:39:54 +0530

    fix: resolve blank screen - fix CreateFund import name mismatch
    CRITICAL BUG FIX:
    - Component exports as 'CreateFund' but was imported as 'Createfund'
    - Case sensitivity mismatch caused component to be undefined
    - Result: blank white screen on /createfund route
    
    CHANGES:
    - App.jsx: Changed import from 'Createfund' to 'CreateFund'
    - App.jsx: Updated Route element to use <CreateFund />
    
    This resolves the blank screen issue on the Create Campaign page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,70 +1,79 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard/dashboard";
-import Createfund from "./pages/createfund/createfund";
-import Display from "./pages/Display/displayfunds";
-import Authenticate from "./pages/Authenticate/Authenticate";
-import Donate from "./pages/Donate/donate";
-import Reactcards from "./pages/Reactcards/reactcards";
-import Crustcreate from "./pages/Crustcreate/crustcreate";
-import ReactGA from "react-ga";
-import $ from "jquery";
-import Home from "./pages/Home/Home";
-import Navbar from "./components/Navbar";
+this.state = {
+  foo: "bar",
+  resumeData: {}
+};
 
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      foo: "bar",
-      resumeData: {}
-    };
-
-    ReactGA.initialize("UA-110570651-1");
-    ReactGA.pageview(window.location.pathname);
+ReactGA.initialize("UA-110570651-1");
+ReactGA.pageview(window.location.pathname);
   }
 
-  getResumeData() {
-    $.ajax({
-      url: "./resumeData.json",
-      dataType: "json",
-      cache: false,
-      success: function (data) {
-        this.setState({ resumeData: data });
-      }.bind(this),
-      error: function (xhr, status, err) {
-        console.log(err);
-        alert(err);
-      }
-    });
+getResumeData() {
+  $.ajax({
+    url: "./resumeData.json",
+    dataType: "json",
+    cache: false,
+    success: function (data) {
+      this.setState({ resumeData: data });
+    }.bind(this),
+    error: function (xhr, status, err) {
+      console.log(err);
+      alert(err);
+    }
+  });
+}
+
+componentDidMount() {
+  this.getResumeData();
+}
+
+this.state = {
+  foo: "bar",
+  resumeData: {}
+};
+
+ReactGA.initialize("UA-110570651-1");
+ReactGA.pageview(window.location.pathname);
   }
 
-  componentDidMount() {
-    this.getResumeData();
-  }
+getResumeData() {
+  $.ajax({
+    url: "./resumeData.json",
+    dataType: "json",
+    cache: false,
+    success: function (data) {
+      this.setState({ resumeData: data });
+    }.bind(this),
+    error: function (xhr, status, err) {
+      console.log(err);
+      alert(err);
+    }
+  });
+}
 
-  render() {
+componentDidMount() {
+  this.getResumeData();
+}
 
-    return (
-      <BrowserRouter>
-        <main className="min-h-screen bg-slate-950">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/createfund" element={<Createfund />}></Route>
-            <Route path="/displayfunds" element={<Display />}></Route>
-            <Route path="/donate" element={<Donate />}></Route>
-            <Route path="/reactcards" element={<Reactcards />}></Route>
-            <Route path="/dashboard" element={<Dashboard />}></Route>
-            <Route path="/crustcreate" element={<Crustcreate />}></Route>
-            <Route path="/Authenticate" element={<Authenticate />}></Route>
-          </Routes>
-        </main>
-      </BrowserRouter>
-    );
-  }
+render() {
+
+  return (
+    <BrowserRouter>
+      <main className="min-h-screen bg-slate-950">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/createfund" element={<CreateFund />}></Route>
+          <Route path="/displayfunds" element={<Display />}></Route>
+          <Route path="/donate" element={<Donate />}></Route>
+          <Route path="/reactcards" element={<Reactcards />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/crustcreate" element={<Crustcreate />}></Route>
+          <Route path="/Authenticate" element={<Authenticate />}></Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+}
 
 }
 export default App;
