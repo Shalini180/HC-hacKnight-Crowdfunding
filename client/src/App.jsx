3f3c2dbf398edbd7248fe2cba3cea2299a3c5bf6
unknown <shalini.a.annadurai@gmail.com>
2025-11-27 11:23:43 +0530

    refactor: rewrite App.jsx as functional component
    - Removed all legacy class-based code (constructor, componentDidMount)
    - Removed ReactGA and jQuery dependencies
    - Fixed syntax errors from previous edits
    - Converted to clean functional component with HashRouter
    - This ensures a stable foundation for the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,63 +8,28 @@ import Authenticate from "./pages/Authenticate/Authenticate";
 import Donate from "./pages/Donate/donate";
 import Reactcards from "./pages/Reactcards/reactcards";
 import Crustcreate from "./pages/Crustcreate/crustcreate";
-import ReactGA from "react-ga";
-import $ from "jquery";
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar";
 
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      foo: "bar",
-      resumeData: {}
-    };
-
-    ReactGA.initialize("UA-110570651-1");
-    ReactGA.pageview(window.location.pathname);
-  }
-
-  getResumeData() {
-    $.ajax({
-      url: "./resumeData.json",
-      dataType: "json",
-      cache: false,
-      success: function (data) {
-        this.setState({ resumeData: data });
-      }.bind(this),
-      error: function (xhr, status, err) {
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
-
-  componentDidMount() {
-    this.getResumeData();
-  }
-
-  render() {
-
-    return (
-      <HashRouter>
-        <main className="min-h-screen bg-slate-950">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/createfund" element={<CreateFund />}></Route>
-            <Route path="/displayfunds" element={<Display />}></Route>
-            <Route path="/donate" element={<Donate />}></Route>
-            <Route path="/reactcards" element={<Reactcards />}></Route>
-            <Route path="/dashboard" element={<Dashboard />}></Route>
-            <Route path="/crustcreate" element={<Crustcreate />}></Route>
-            <Route path="/Authenticate" element={<Authenticate />}></Route>
-          </Routes>
-        </main>
-      </HashRouter>
-    );
-  }
-
+// Clean Functional Component - No Legacy Code
+function App() {
+  return (
+    <HashRouter>
+      <main className="min-h-screen bg-slate-950">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/createfund" element={<CreateFund />} />
+          <Route path="/displayfunds" element={<Display />} />
+          <Route path="/donate" element={<Donate />} />
+          <Route path="/reactcards" element={<Reactcards />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/crustcreate" element={<Crustcreate />} />
+          <Route path="/Authenticate" element={<Authenticate />} />
+        </Routes>
+      </main>
+    </HashRouter>
+  );
 }
+
 export default App;
