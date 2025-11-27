5971e79afc9aa08d9bd93a9b7be30f3b76e1d004
unknown <shalini.a.annadurai@gmail.com>
2025-11-27 11:24:53 +0530

    fix: binary search - comment out all other routes
    - Commented out Dashboard, Display, Authenticate, Donate, Reactcards, Crustcreate
    - Only Home and CreateFund remain active
    - This determines if one of the unused components is crashing the app on load

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./App.css";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard/dashboard";
+// import Dashboard from "./pages/Dashboard/dashboard";
 import CreateFund from "./pages/createfund/createfund";
-import Display from "./pages/Display/displayfunds";
-import Authenticate from "./pages/Authenticate/Authenticate";
-import Donate from "./pages/Donate/donate";
-import Reactcards from "./pages/Reactcards/reactcards";
-import Crustcreate from "./pages/Crustcreate/crustcreate";
+// import Display from "./pages/Display/displayfunds";
+// import Authenticate from "./pages/Authenticate/Authenticate";
+// import Donate from "./pages/Donate/donate";
+// import Reactcards from "./pages/Reactcards/reactcards";
+// import Crustcreate from "./pages/Crustcreate/crustcreate";
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar";
 
-// Clean Functional Component - No Legacy Code
+// Binary Search: Minimal Imports
 function App() {
   return (
     <HashRouter>
@@ -20,12 +20,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/createfund" element={<CreateFund />} />
-          <Route path="/displayfunds" element={<Display />} />
-          <Route path="/donate" element={<Donate />} />
-          <Route path="/reactcards" element={<Reactcards />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/crustcreate" element={<Crustcreate />} />
-          <Route path="/Authenticate" element={<Authenticate />} />
+          {/* <Route path="/displayfunds" element={<Display />} /> */}
+          {/* <Route path="/donate" element={<Donate />} /> */}
+          {/* <Route path="/reactcards" element={<Reactcards />} /> */}
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          {/* <Route path="/crustcreate" element={<Crustcreate />} /> */}
+          {/* <Route path="/Authenticate" element={<Authenticate />} /> */}
         </Routes>
       </main>
     </HashRouter>
